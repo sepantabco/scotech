cdb19a1d28540531e329aa16396a42bbec5b8211
mehdi_heydarzade <ariyaware@hotmail.com>
2019-12-04 16:50:44 +0330

    safhe hafari

--- libraries/MiningPage/MiningPage.js
+++ libraries/MiningPage/MiningPage.js
@@ -1,152 +1,149 @@
-import * as React from 'react';
-import {
-    Text,
-    View,
-    StyleSheet,
-    WebView,
-    ImageBackground,
-    Animated,
-    TouchableOpacity,
-    ScrollView,
-    Image,
-    AsyncStorage, Alert
-} from 'react-native';
-import FooterViewI from "../FooterViewI";
-import {P_URL} from "../PUBLICURLs";
-class FadeInView extends React.Component {
-    state = {
-        fadeAnim: new Animated.Value(0),  // Initial value for opacity: 0
-    };
+import React, { Component } from 'react'
+import { Text, View, ScrollView, Image, FlatList } from 'react-native'
+import FooterViewI from '../FooterViewI'
 
-    componentDidMount() {
-        Animated.timing(                  // Animate over time
-            this.state.fadeAnim,            // The animated value to drive
-            {
-                toValue: 1,                   // Animate to opacity: 1 (opaque)
-                duration: 3000,              // Make it take a while
-            }
-        ).start();                        // Starts the animation
-    }
-
-    render() {
-        let { fadeAnim } = this.state;
-
-        return (
-            <Animated.View                 // Special animatable View
-                style={{
-                    ...this.props.style,
-                    opacity: fadeAnim,         // Bind opacity to animated value
-                }}
-            >
-                {this.props.children}
-            </Animated.View>
-        );
-    }
-}
-export default class MiningPage extends React.Component {
-    constructor(){
+export class MiningPage extends Component {
+    constructor() {
         super();
         this.state = {
-            backgrounds : require('../../images/001.jpg'),
-            down : 0.1,
-            username : "",
-            img : 1
-        }
-    }
-    async getUsername() {
-        try {
-            let token = await AsyncStorage.getItem('username');
-            return token;
-        } catch (error) {
-            Alert.alert(error.toString());
-        }
-    }
-    _set_username(username){
-        this.setState({username:username})
-    }
-    async componentDidMount(){
-        let username = await this.getUsername();
-        await this._set_username(username);
-        setInterval(() => {
-            this._set_state()
-        },3000);
-    }
-    _set_state(){
-        if(this.state.img < 9){
-            let imn = this.state.img + 1;
-            this.setState({img : imn})
-        }else{
-            this.setState({img : 1})
-        }
-        let bc = (this.state.img == 1 ) ? require('../../images/001.jpg') :
-            (this.state.img == 2 ) ? require('../../images/002.jpg') :
-                (this.state.img == 3 ) ? require('../../images/003.jpg') :
-                    (this.state.img == 4 ) ? require('../../images/004.jpg') :
-                        (this.state.img == 5 ) ? require('../../images/005.jpg') :
-                            (this.state.img == 6 ) ? require('../../images/006.jpg') : require('../../images/001.jpg');
-        this.setState({backgrounds : bc})
+            leagueData: [
+                { id: 1, day: 2, hour: 8, minute: 24, second: 20, gameName: 'PacMan', Moon: 'شهریورماه' },
 
+            ],
+            gameData: [
+                { id: 1, title: 'Mario' }
+            ]
+        }
     }
-
+    componentDidMount
     render() {
         return (
-            <View style={{flex:1}}>
-                <ScrollView>
-                    <ImageBackground source={this.state.backgrounds} style={{resizeMethod: 'scale'}}>
-                        <View style={{flexDirection:'column',justifyContent:'space-between',marginTop:20}}>
-                            <View style={{flexDirection:'row',justifyContent:'space-between',marginBottom:200}}>
-                                <TouchableOpacity style={{marginTop:20}} onPress={() => this.props.navigation.navigate('webview',{url : P_URL + 'vote?username=' + this.state.username})}>
-                                    <ImageBackground source={require('../../images/vote_background.png')} style={{height:120,width:120,marginLeft:10}} >
-                                    </ImageBackground>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={{marginTop:20}}
-                                                  onPress={() => this.props.navigation.navigate('gamecenter')}>
-                                    <ImageBackground source={require('../../images/game_background.png')} style={{height:120,width:120,marginRight:10}} >
-                                    </ImageBackground>
-                                </TouchableOpacity>
-                            </View>
-                            <View style={{flexDirection:'row',justifyContent:'space-between',paddingBottom:100}}>
-                                <TouchableOpacity style={{marginTop:20}}
-                                                  onPress={() => this.props.navigation.navigate('webview',{url : P_URL+'quiz?username=' + this.state.username})}>
-                                    <ImageBackground source={require('../../images/my_quiz.png')} style={{height:120,width:120,marginLeft:10}} >
-                                    </ImageBackground>
-                                </TouchableOpacity>
-                                <TouchableOpacity style={{marginTop:20}} onPress={() => this.props.navigation.navigate('webview',{url : P_URL + 'others?username=' + this.state.username})}>
-                                    <ImageBackground source={require('../../images/others_background.png')} style={{height:120,width:120,marginRight:15}}>
-                                    </ImageBackground>
-                                </TouchableOpacity>
-                            </View>
+            <View style={{ flex: 1 }}>
+                <ScrollView style={{ flex: 1, backgroundColor: 'white' }}>
+                    {/* فلکس آواتار و امتیاز */}
+                    <View style={{ height: 280, borderBottomWidth: .5, borderColor: 'black' }}>
+                        {/* فلکس آواتار و اسم */}
+                        <View style={{ flex: 2.5, alignItems: 'center', justifyContent: 'center' }}>
+                            <Image style={{ height: 100, width: 100 }} source={require('./../../images/person.png')} />
+                            <View style={{ justifyContent: 'center', alignItems: 'center', height: 15, width: 30, backgroundColor: "#9720d2", borderRadius: 10, marginTop: -7.5 }}><Text style={{ fontFamily: 'IRANSans(FaNum)' }}>12</Text></View>
+                            <Text style={{ fontSize: 16 }}>NickName</Text>
                         </View>
-                    </ImageBackground>
-                </ScrollView>
-                <FooterViewI menu={4} navigation={this.props.navigation}/>
-            </View>
-        );
+                        {/*end فلکس آواتار و اسم */}
+                        {/* فلکس امتیازات و دکمه های مدال و جدول */}
+                        <View style={{ flex: 2 }}>
+                            {/* فکلس امتیازات */}
+                            <View style={{ flex: 1, flexDirection: 'row-reverse' }}>
+                                <View style={{ flex: 1.2, alignItems: 'center', paddingTop: 6 }}>
+                                    <View style={{ flexDirection: 'row-reverse', height: 30, width: '90%', backgroundColor: '#ffd83b', borderRadius: 15, justifyContent: 'space-around', alignItems: 'center' }}>
+                                        <Image style={{ height: 15, width: 15 }} source={require('../../images/logos/starpoint.png')}></Image>
+                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14, color: 'black' }}>8000</Text>
+                                    </View>
+                                </View>
+                                <View style={{ flex: 2.8, paddingTop: 6, alignItems: 'center' }}>
+                                    <View style={{ height: 30, width: '90%', backgroundColor: '#e6e6e6', borderRadius: 15, flexDirection: 'row-reverse', alignItems: 'center', justifyContent: 'space-between' }}>
+                                        <View style={{ height: 30, width: '70%', backgroundColor: "#9720d2", borderRadius: 15, flexDirection: 'row-reverse', alignItems: 'center', justifyContent: 'space-between', position: "absolute" }}></View>
+                                        <Text style={{ marginHorizontal: 8, fontFamily: 'IRANSans(FaNum)', fontSize: 14, color: 'white' }}>12</Text>
+                                        <Text style={{ marginHorizontal: 8, fontFamily: 'IRANSans(FaNum)', fontSize: 14, color: 'black' }}>13</Text>
+                                    </View>
+                                    <View style={{ flexDirection: 'row-reverse' }}>
+                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14, color: '#9720d2' }}>3000 امتیاز</Text>
+                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14, color: '#858585' }}> تا مرحله 13</Text>
+                                    </View>
+                                </View>
+                            </View>
+                            {/*end فکلس امتیازات */}
+                            {/* فلکس دکمه های مدال و جدول */}
+                            <View style={{ flex: 1, flexDirection: 'row-reverse', justifyContent: 'space-around', alignItems: 'center' }}>
+                                <View style={{ height: 35, width: '45%', borderRadius: 12, borderWidth: .5, borderColor: 'black', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontFamily: 'IRANSansMobile', fontSize: 14 }}>جدول امتیازات</Text></View>
+                                <View style={{ height: 35, width: '45%', borderRadius: 12, borderWidth: .5, borderColor: 'black', justifyContent: 'center', alignItems: 'center' }}><Text style={{ fontFamily: 'IRANSansMobile', fontSize: 14 }}>مدال‌ها</Text></View>
+                            </View>
+                            {/*end فلکس دکمه های مدال و جدول */}
+                        </View>
+                        {/*end فلکس امتیازات و دکمه های مدال و جدول */}
+                    </View>
+                    {/*end فلکس آواتار و امتیاز */}
+                    {/* تیتر حفاری */}
+                    <View style={{ height: 50, justifyContent: 'center' }}><Text style={{ marginHorizontal: 15, color: 'black', fontFamily: 'IRANSansMobile', fontSize: 25 }}>حفاری</Text></View>
+                    {/*end تیتر حفاری */}
+                    {/* فلکس لیگها */}
+                    <View>
+                        <View style={{ height: 50, justifyContent: 'center' }}><Text style={{ color: 'black', marginHorizontal: 15, fontFamily: 'IRANSansMobile', fontSize: 18 }}>لیگ‌ها</Text></View>
+                        {/* فلکس کارتهای لیگ */}
+                        <FlatList
+                            data={this.state.leagueData}
+                            horizontal
+                            renderItem={({ item }) =>
+                                <View>
+                                    <View style={{ flex: 1, height: 200, width: 250, backgroundColor: 'white', borderRadius: 20, borderWidth: 1, borderColor: '#e4e4e4', marginEnd: 20 }}>
+                                        {/* بالای کارت */}
+                                        <View style={{ flex: 3, backgroundColor: '#e4e4e4', borderTopEndRadius: 20, borderTopStartRadius: 20 }}></View>
+                                        {/*end بالای کارت */}
+                                        {/* پایین کارت */}
+                                        <View style={{ flex: 2, paddingHorizontal: 20, justifyContent: 'space-around' }}>
+                                            <View><Text style={{ fontFamily: 'IRANSansMobile', fontSize: 14 }}>لیگ {item.Moon} {item.gameName}</Text></View>
+                                            {/* زمان باقیمانده */}
+                                            <View style={{ flexDirection: 'row-reverse', justifyContent: 'space-around' }}>
+                                                <View style={{ flex: 1 }}>
+                                                    <Text style={{ fontFamily: 'IRANSansMobile', fontSize: 14 }}>زمان باقیمانده</Text>
+                                                </View>
+                                                <View style={{ flex: 2, flexDirection: 'row-reverse', justifyContent: 'space-around' }}>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>ثانیه</Text>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>{item.second}</Text>
+                                                    </View>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>دقیقه</Text>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>{item.minute}</Text>
+                                                    </View>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>ساعت</Text>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>{item.hour}</Text>
+                                                    </View>
+                                                    <View style={{ justifyContent: 'center', alignItems: 'center' }}>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>روز</Text>
+                                                        <Text style={{ fontFamily: 'IRANSans(FaNum)', fontSize: 14 }}>{item.day}</Text>
+                                                    </View>
+                                                </View>
+                                            </View>
+                                            {/*end زمان باقیمانده */}
+                                        </View>
+                                        {/*end پایین کارت */}
+                                    </View>
+                                </View>
+                            } />
+                        {/*end فلکس کارتهای لیگ */}
+                    </View>
+                    {/*end فلکس لیگها */}
+                    {/* فلکس بازیها */}
+                    <View>
+                        <View style={{ height: 50, justifyContent: 'center' }}><Text style={{ color: 'black', marginHorizontal: 15, fontFamily: 'IRANSansMobile', fontSize: 18 }}>بازی‌ها</Text></View>
+                        {/* فلکس کارتهای بازی */}
+                        <FlatList
+                            data={this.state.gameData}
+                            renderItem={({ item }) =>
+                                <View>
+                                    <View style={{ flex: 1, height: 200, width: 200, backgroundColor: 'white', borderRadius: 20, borderWidth: 1, borderColor: '#e4e4e4', marginEnd: 20 }}>
+                                        {/* بالای کارت */}
+                                        <View style={{ flex: 3, backgroundColor: '#e4e4e4', borderTopEndRadius: 20, borderTopStartRadius: 20 }}></View>
+                                        {/*end بالای کارت */}
+                                        {/* پایین کارت */}
+                                        <View style={{ flex: 1, paddingHorizontal: 20, justifyContent: 'space-around' }}>
+                                            <Text style={{ fontFamily: 'IRANSansMobile', fontSize: 14 }}>{item.title}</Text>
+                                        </View>
+                                        {/*end پایین کارت */}
+
+                                    </View>
+                                </View>
+                                // {/*end فلکس کارتهای بازی */}
+                            }
+                        />
+                    </View>
+                    {/*end فلکس بازیها */}
+                </ScrollView >
+                <FooterViewI menu={4} navigation={this.props.navigation} />
+            </View >
+        )
     }
 }
 
-const styles = StyleSheet.create({
-    footerViews:{
-        alignItems:'center',
-        justifyContent: 'center'
-    },
-    container: {
-        flex: 1,
-        flexDirection: 'row'
-    },
-    welcome: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-    },
-    scroll: {
-        flex: 1,
-        backgroundColor: '#f0f0f0',
-        margin: 10,
-    },
-    instructions: {
-        textAlign: 'center',
-        color: '#333333',
-        marginBottom: 5,
-    },
-});
+export default MiningPage
