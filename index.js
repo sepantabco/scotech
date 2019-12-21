d679a36c4f5eec549ca071a6358c3c6cd70cf65b
Mehdi Heydarzadeh <ariyaware@hotmail.com>
2019-12-21 11:16:07 +0330

    merge conflict

--- index.js
+++ index.js
@@ -6,8 +6,4 @@
 import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
-import CompeleteMenue from './libraries/CategoryPage/CompeleteMenue';
-import CategoryADs from './libraries/CategoryADs/CategoryADs';
-import Webview from './libraries/Webview';
-
 AppRegistry.registerComponent(appName, () => App); 
