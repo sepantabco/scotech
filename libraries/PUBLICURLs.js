960b992d784ec25ce67d6be51b45d6e2c49950e9
Mehdi Heydarzadeh <ariyaware@hotmail.com>
2020-01-03 21:52:46 +0330

    SearchPage

--- libraries/PUBLICURLs.js
+++ libraries/PUBLICURLs.js
@@ -1,12 +1,10 @@
 // let get_url;
 // export let P_URL = "http://192.168.1.5:8000/requests/";
-<<<<<<< HEAD
-=======
+
 let get_url;
 export let P_URL = "http://192.168.1.14:8000/requests/"; 
 export let L_URL = "https://shop.ghiasi.me/api/";
 export let I_URL = "https://shop.ghiasi.me/images/"
->>>>>>> b00464eced39df2f9cdea01d9c15836cf605b0c5
 // let get_url;
 // export let P_URL = "http://192.168.1.14:8000/requests/"; 
 let get_url;
