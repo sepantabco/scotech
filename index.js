b3f5967ba4fa6ecf5e0b5edc602d382f0bdc6059
Mehdi Heydarzadeh <ariyaware@hotmail.com>
2020-01-05 23:52:05 +0330

    a

--- index.js
+++ index.js
@@ -4,13 +4,9 @@
  */
 import { AppRegistry, YellowBox,I18nManager } from 'react-native';
 console.disableYellowBox=true
-import App from './App';
-import { name as appName } from './app.json';
-import StartPage from './libraries/StartPage';
-import MainRoute from './libraries/MainRoute';
-import SplashScreen from './libraries/SplashScreen';
-import Search from './libraries/Search';
 I18nManager.allowRTL(false);
 I18nManager.forceRTL(false)
+import { name as appName } from './app.json';
+import MainRoute from './libraries/MainRoute';
 
 AppRegistry.registerComponent(appName, () =>MainRoute); 
